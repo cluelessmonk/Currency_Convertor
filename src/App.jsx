39ab07ecfea5713b495f5a476d9e1d07db71bcaf
Umang <neverstopwithme@gmail.com>
2024-01-24 14:52:01 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
-
+import { InputBox } from './components'
 import './App.css'
+import useCurrencyInfo from './hooks/useCurrencyinfo'
 
 function App() {
-
+const [amount ,setAmount]=useState(0);
+const [from,setFrom]=useState("usd");
+const [to,setTo]=useState("inr");
+const [convertedAmount,setConvertedAmount]=useState(0);
+const currencyInfo=useCurrencyInfo(from)
+const options=Object.keys(currencyInfo)
+const swap=()=>{
+     const temp=from
+     from=to
+     to=temp
+     setFrom(from)
+     setTo(to)
+     setConvertedAmount()
+} 
 
   return (
     <>
       
-<h1 className="text-3xl bg-orange-600 ">Currency Convertor</h1>
+            <h1 className="text-3xl bg-orange-600 ">Currency Convertor</h1>
 
     </>
   )
--- src/hooks/useCurrencyinfo.js
+++ src/hooks/useCurrencyinfo.js
@@ -7,10 +7,9 @@ function useCurrencyInfo(currency) {
     )
       .then((res) => res.json())
       .then((res) => setData(res[currency]));
+
     console.log(data);
   }, [currency]);
-  return data
-  
+  return data;
 }
 export default useCurrencyInfo;
-
